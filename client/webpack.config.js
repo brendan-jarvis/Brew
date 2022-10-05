ddbf1cc5317940502c59f40be3b10834fd470849
Brendan Jarvis <jarvisbrendanj@gmail.com>
2022-10-05 20:21:16 +1300

    Update dependencies

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path')
 
+const webpack = require('webpack')
+require('dotenv').config()
+
 module.exports = {
   entry: ['./client/index.js'],
   output: {
@@ -7,6 +10,15 @@ module.exports = {
     filename: 'bundle.js',
   },
   mode: 'development',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        AUTH0_DOMAIN: JSON.stringify(process.env.AUTH0_DOMAIN),
+        AUTH0_CLIENT_ID: JSON.stringify(process.env.AUTH0_CLIENT_ID),
+        AUTH0_AUDIENCE: JSON.stringify(process.env.AUTH0_AUDIENCE),
+      },
+    }),
+  ],
   module: {
     rules: [
       {
