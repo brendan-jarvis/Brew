0ef3b3b5a5781dfd4ede3b0cb13629f952f1a48c
Brendan Jarvis <jarvisbrendanj@gmail.com>
2022-10-14 11:49:58 +1300

    Fix error on refresh

--- server/server.js
+++ server/server.js
@@ -6,8 +6,12 @@ const server = express()
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
-server.get('*', (req, res) => {
-  res.sendFile(path.resolve('server/public/index.html'))
+server.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public/index.html'), function (err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
 })
 
 module.exports = server
